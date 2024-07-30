be7f4c9cfed2133d7497e66953041848d2aa164e
Praful Mathur <praful.mathur@gmail.com>
2024-07-30 14:28:02 -0700

    Add a period to block apps during a cool off period

--- distraction_reduction.py
+++ distraction_reduction.py
@@ -17,17 +17,25 @@ SYSTEM_PROCESSES = [
     "loginwindow",
 ]
 ALLOWED_PROCESSES = [
-    # Enter processes you want to keep here in lower case
+COOL_OFF_APPS = [
+    # Add cool off apps you want to prevent using immediately after
 ]
-TIMER_COUNTDOWN = 600
+TIMER_COUNTDOWN = 60 * 10
+COOL_OFF_TIMER_COUNTDOWN = 60 * 15
 
 
 class CountdownApp(object):
     def __init__(self):
-        self.config = {"app_name": "Countdown", "interval": TIMER_COUNTDOWN}
+        self.config = {
+           "app_name": "Countdown", 
+           "interval": TIMER_COUNTDOWN,
+           "cooloff_interval": COOL_OFF_TIMER_COUNTDOWN
+        }
         self.app = rumps.App(self.config["app_name"])
         self.timer = rumps.Timer(self.on_tick, 1)
         self.interval = self.config["interval"]
+        self.cool_off = False
+        self.cool_off_interval = self.config["cooloff_interval"]
         self.start_timer()
 
     def on_tick(self, sender):
@@ -35,16 +43,24 @@ class CountdownApp(object):
         mins = time_left // 60 if time_left >= 0 else time_left // 60 + 1
         secs = time_left % 60 if time_left >= 0 else (-1 * time_left) % 60
 
+        if self.cool_off:
+            for app_name in COOL_OFF_APPS:
+                if is_app_running(app_name):
+                    kill_app(app_name)
+
         if mins == 0 and time_left < 0:
             terminate_unallowed_foreground_processes()
+            self.cool_off = not self.cool_off
             self.start_timer()
         else:
             self.app.title = "{:2d}:{:02d}".format(mins, secs)
+            if self.cool_off:
+                self.app.title = "❄" + self.app.title
             sender.count += 1
 
     def start_timer(self):
         self.timer.count = 0
-        self.timer.end = self.interval
+        self.timer.end = self.cool_off_interval if self.cool_off else self.interval
         self.timer.start()
 
     def run(self):
@@ -77,6 +93,30 @@ def terminate_unallowed_foreground_processes():
                     proc.wait()  # Wait for process termination
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
+
+def is_app_running(app_name):
+    try:
+        # Check if the application is running
+        result = subprocess.run(
+            ['pgrep', '-f', app_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        return result.returncode == 0
+    except Exception as e:
+        print(f"An error occurred while checking the application: {e}")
+        return False
+
+def kill_app(app_name):
+    try:
+        # Kill the application
+        subprocess.run(['pkill', '-f', app_name], check=True)
+        print(f"Blocking {app_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while killing the application: {e}")
+
+        
 
 
 if __name__ == "__main__":
